ff530c997eb3d4735248ba9d9459071cfb04bb59
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-07-13 12:14:07 +0200

    Sync write a few files to put into zip, make that async next

--- build-backend.js
+++ build-backend.js
@@ -1,36 +1,47 @@
 /*jshint node: true */
 "use strict";
 
-var spawn = require( "child_process" ).spawn;
+var spawn = require( "child_process" ).spawn,
+	fs = require( "fs" ),
+	rimraf = require( "rimraf" ),
+	async = require( "async" );
 
 function Builder( fields ) {
 	this.fields = fields;
 }
 Builder.prototype = {
-	filelist: function() {
-		var prefix = "minified/jquery.ui.",
-			postfix = ".min.js",
-			list = [];
+	build: function( callback ) {
+		var tmpdir = "tmp" + (+new Date()),
+			target = "jquery-ui-custom",
+			targetdir = tmpdir + "/" + target + "/";
+		fs.mkdirSync( tmpdir );
+		fs.mkdirSync( targetdir );
+		fs.mkdirSync( targetdir + "minified" );
+		fs.writeFileSync( targetdir + "version.txt", "custom" );
 		this.fields.forEach(function( field ) {
-			list.push( prefix + field + postfix );
+			var file = "minified/jquery.ui." + field + ".min.js";
+			fs.writeFileSync( targetdir + file, fs.readFileSync( "dist/" + file ) );
 		});
-		return list;
+		callback( tmpdir, target );
 	},
 	writeTo: function( response, callback ) {
 		var that = this;
-		var child = spawn( "zip", [ "-r", "-" ].concat( this.filelist() ), { cwd: "dist" } );
-		child.stdout.on( "data", function( data) {
-			response.write( data );
-		});
-		child.stderr.on( "data", function( data) {
-			console.error( data.toString() );
-		});
-		child.on( "exit", function( code ) {
-			if ( code !== 0 ) {
-				callback( "zip failed :(" );
-				return;
-			}
-			callback( null, "All good!" );
+		this.build(function( cwd, target ) {
+			var child = spawn( "zip", [ "-r", "-", target ], { cwd: cwd } );
+			child.stdout.on( "data", function( data) {
+				response.write( data );
+			});
+			child.stderr.on( "data", function( data) {
+				console.error( data.toString() );
+			});
+			child.on( "exit", function( code ) {
+				rimraf.sync( cwd );
+				if ( code !== 0 ) {
+					callback( "zip failed :(" );
+					return;
+				}
+				callback( null, "All good!" );
+			});
 		});
 	}
 };
