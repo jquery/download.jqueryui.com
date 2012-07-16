8f57c38b18705b47921c2e8935ddf5f53238c487
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-07-16 19:37:30 +0200

    Add checkboxes to toggle all or just each category

---
+++ app/resources/frontend.js
@@ -0,0 +1,7 @@
+$( "label.toggle input" ).click(function() {
+	var inputs = $( this ).parent().nextUntil( ".toggle" ).find( "input[type=checkbox]" );
+	inputs.attr( "checked", !!$( this ).attr( "checked" ) );
+});
+$( "label.toggleAll input" ).click(function() {
+	$( this ).parent().parent().find( "input[type=checkbox]" ).not( this ).attr( "checked", !!$( this ).attr( "checked" ) );
+});
