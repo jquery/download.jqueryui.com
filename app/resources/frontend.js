89e470f371f7fae8fc46696afb864a2037c2279b
Rafael Xavier de Souza <rxaviers@gmail.com>
2012-07-17 02:36:37 -0300

    Handling dependencies on frontend

--- app/resources/frontend.js
+++ app/resources/frontend.js
@@ -1,12 +1,71 @@
 ;
 (function( $, undefined ) {
 
-    $( "label.toggle input" ).click(function() {
-		var inputs = $( this ).parent().nextUntil( ".toggle" ).find( "input[type=checkbox]" );
-		inputs.attr( "checked", !!$( this ).attr( "checked" ) );
+	var checkboxes,
+		dependencies = {},
+		dependents = {};
+
+	function check( elem, value ) {
+		elem.each(function() {
+			var elem = $( this ),
+				name = elem.attr( "name" );
+
+			// Handle dependencies
+			if ( name ) {
+				// Whenever a checkbox is activated, also activate all parent dependencies
+				if ( value ) { 
+					if ( dependencies[ name ] ) {
+						check( dependencies[ name ], value );
+					}
+				} else if ( dependents[ name ] ) {
+					// Whenever a checkbox is deactivated, also deactivate all child dependencies
+					check( dependents[ name ], value );
+				}
+			}
+
+			elem.prop( "checked", value );
+		});
+	}
+
+
+	/**
+	 * Init
+	 */
+	checkboxes = $( ".download-builder input[type=checkbox]" );
+	checkboxes.each(function() {
+		var checkbox = $( this ),
+			thisDependencies = checkbox.data( "dependencies" ),
+			thisName = checkbox.attr( "name" );
+
+		if ( !thisName || !thisDependencies ) {
+			return;
+		}
+		thisDependencies = thisDependencies.split( "," );
+		dependencies[ thisName ] = $();
+		$.each( thisDependencies, function() {
+			var dependecy = this,
+				dependecyElem = $( "[name=" + this + "]" );
+			dependencies[ thisName ] = dependencies[ thisName ].add( dependecyElem );
+			if ( !dependents[ dependecy ] ) {
+				dependents[ dependecy ] = $();
+			}
+			dependents[ dependecy ] = dependents[ dependecy ].add( checkbox );
+		});
 	});
-    $( "label.toggleAll input" ).click(function() {
-		$( this ).parent().parent().find( "input[type=checkbox]" ).not( this ).attr( "checked", !!$( this ).attr( "checked" ) );
+
+
+	/**
+	 * Bind
+	 */
+	checkboxes.click(function( event ) {
+		var target = $( event.target );
+		if ( target.parent().is( ".toggle" ) ) {
+			check( $( this ).parent().nextUntil( ".toggle" ).find( "input[type=checkbox]" ), $( this ).prop( "checked" ) );
+		} else if ( target.parent().is( ".toggleAll" ) ) {
+			check( $( this ).closest( "form" ).find( "input[type=checkbox]" ).not( this ), $( this ).prop( "checked" ) );
+		} else {
+			check( $( this ), $( this ).prop( "checked" ) );
+		}
 	});
 
 })( jQuery );
