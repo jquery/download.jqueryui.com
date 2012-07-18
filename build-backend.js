b7506fe1fde9219e1b6fe4acd8af6e10f887fb88
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-07-18 13:06:11 +0200

    Generate most of the zip files content. Just sync, no theme, cheap concat, no proper minifying

--- build-backend.js
+++ build-backend.js
@@ -11,6 +11,7 @@ var spawn = require( "child_process" ).spawn,
 var indexTemplate = handlebars.compile( fs.readFileSync( "zip-index.html", "utf8" ) );
 
 function Builder( fields ) {
+	// TODO make sure fields are ordered based on dependencies
 	this.fields = fields;
 }
 Builder.prototype = {
@@ -18,11 +19,31 @@ Builder.prototype = {
 	build: function( callback ) {
 		var tmpdir = "tmp" + (+new Date()),
 			target = "jquery-ui-custom",
+			input = "versions/jquery-ui-1.9.0pre",
 			targetdir = tmpdir + "/" + target + "/";
 		fs.mkdirSync( tmpdir );
 		fs.mkdirSync( targetdir );
 		fs.mkdirSync( targetdir + "minified" );
-		fs.writeFileSync( targetdir + "version.txt", "custom" );
+
+		// TODO exclude or filter *.jquery.json files
+		fs.mkdirSync( targetdir + "development-bundle" );
+		glob( "versions/jquery-ui-1.9.0pre/*.*" ).forEach(function( file ) {
+			fs.writeFileSync( targetdir + file.replace(input, "development-bundle"), fs.readFileSync( file ) );
+		});
+
+		fs.mkdirSync( targetdir + "development-bundle/external" );
+		glob( input + "/external/*" ).forEach(function( file ) {
+			fs.writeFileSync( targetdir + file.replace(input, "development-bundle"), fs.readFileSync( file ) );
+		});
+
+		fs.mkdirSync( targetdir + "development-bundle/demos" );
+		fs.writeFileSync( targetdir + "development-bundle/demos/demos.css", fs.readFileSync( input + "/demos/demos.css" ) );
+		fs.writeFileSync( targetdir + "development-bundle/demos/index.html", fs.readFileSync( input + "/demos/index.html" ) );
+		fs.mkdirSync( targetdir + "development-bundle/demos/images" );
+		glob( input + "/demos/images/*" ).forEach(function( file ) {
+			fs.writeFileSync( targetdir + file.replace(input, "development-bundle"), fs.readFileSync( file ) );
+		});
+
 		var meta = {
 			jquery: {
 				version: "1.7.2"
@@ -31,26 +52,50 @@ Builder.prototype = {
 				version: "1.9.0"
 			}
 		};
+		fs.mkdirSync( targetdir + "development-bundle/ui" );
+		fs.mkdirSync( targetdir + "development-bundle/ui/minified" );
+		// TODO replicate banner generation from grunt build
+		var concatFiles = "/* jQuery UI custom */\n";
 		this.fields.forEach(function( field ) {
-			var file = "minified/jquery.ui." + field + ".min.js";
 			meta.ui[ field ] = true;
-			fs.writeFileSync( targetdir + file, fs.readFileSync( "versions/jquery-ui-1.9.0pre/ui/" + file ) );
+
+			var file = "ui/jquery.ui." + field + ".js";
+			var content = fs.readFileSync( input + "/" + file );
+			// TODO strip banner
+			concatFiles += content;
+			fs.writeFileSync( targetdir + "development-bundle/" + file, content );
+
+			var minfile = "ui/minified/jquery.ui." + field + ".min.js";
+			fs.writeFileSync( targetdir + "development-bundle/" + minfile, fs.readFileSync( input + "/" + minfile ) );
+
+			if ( fs.existsSync( input + "/demos/" + field ) ) {
+				fs.mkdirSync( targetdir + "development-bundle/demos/" + field );
+				glob( input + "/demos/" + field + "/**" ).forEach(function( file ) {
+					if ( fs.statSync( file ).isDirectory() ) {
+						fs.mkdirSync( targetdir + file.replace(input, "development-bundle") );
+					} else {
+						fs.writeFileSync( targetdir + file.replace(input, "development-bundle"), fs.readFileSync( file ) );
+					}
+				});
+			}
 		});
+		fs.writeFileSync( targetdir + "development-bundle/ui/jquery-ui-" + meta.ui.version + ".custom.js", concatFiles );
+
 		fs.writeFileSync( targetdir + "index.html", indexTemplate( meta ) );
 		fs.mkdirSync( targetdir + "js" );
-		fs.writeFileSync( targetdir + "js/jquery-" + meta.jquery.version + ".js", fs.readFileSync( "versions/jquery-ui-1.9.0pre/jquery-" + meta.jquery.version + ".js" ) );
+		fs.writeFileSync( targetdir + "js/jquery-" + meta.jquery.version + ".js", fs.readFileSync( input + "/jquery-" + meta.jquery.version + ".js" ) );
 
-		fs.writeFileSync( targetdir + "js/jquery-ui-" + meta.ui.version + ".custom.js", fs.readFileSync( "versions/jquery-ui-1.9.0pre/ui/jquery-ui.js" ) );
-		fs.writeFileSync( targetdir + "js/jquery-ui-" + meta.ui.version + ".custom.min.js", fs.readFileSync( "versions/jquery-ui-1.9.0pre/ui/minified/jquery-ui.min.js" ) );
+		fs.writeFileSync( targetdir + "js/jquery-ui-" + meta.ui.version + ".custom.js", fs.readFileSync( input + "/ui/jquery-ui.js" ) );
+		fs.writeFileSync( targetdir + "js/jquery-ui-" + meta.ui.version + ".custom.min.js", fs.readFileSync( input + "/ui/minified/jquery-ui.min.js" ) );
 
 		fs.mkdirSync( targetdir + "css" );
 		fs.mkdirSync( targetdir + "css/base" );
-		fs.writeFileSync( targetdir + "css/base/jquery-ui-" + meta.ui.version + ".custom.css", fs.readFileSync( "versions/jquery-ui-1.9.0pre/themes/base/jquery-ui.css" ) );
-		fs.writeFileSync( targetdir + "css/base/jquery-ui-" + meta.ui.version + ".custom.min.css", fs.readFileSync( "versions/jquery-ui-1.9.0pre/themes/base/minified/jquery-ui.min.css" ) );
+		fs.writeFileSync( targetdir + "css/base/jquery-ui-" + meta.ui.version + ".custom.css", fs.readFileSync( input + "/themes/base/jquery-ui.css" ) );
+		fs.writeFileSync( targetdir + "css/base/jquery-ui-" + meta.ui.version + ".custom.min.css", fs.readFileSync( input + "/themes/base/minified/jquery-ui.min.css" ) );
 
 		fs.mkdirSync( targetdir + "css/base/images" );
-		glob( "versions/jquery-ui-1.9.0pre/themes/base/images/*" ).forEach(function( file ) {
-			fs.writeFileSync( targetdir + file.replace("versions/jquery-ui-1.9.0pre/themes", "css"), fs.readFileSync( file ) );
+		glob( input + "/themes/base/images/*" ).forEach(function( file ) {
+			fs.writeFileSync( targetdir + file.replace(input + "/themes", "css"), fs.readFileSync( file ) );
 		});
 
 		callback( tmpdir, target );
@@ -63,7 +108,7 @@ Builder.prototype = {
 				response.write( data );
 			});
 			child.stderr.on( "data", function( data) {
-				console.error( data.toString() );
+				// console.error( data.toString() );
 			});
 			child.on( "exit", function( code ) {
 				rimraf.sync( cwd );
--- cli-test.js
+++ cli-test.js
@@ -4,6 +4,11 @@ var components = require( "./manifest" ).components().map(function( component ) 
 	return component.name;
 });
 
+// specify any additional arguments to include only a subset of components
+if (process.argv.length > 2) {
+	components = components.slice(0, 5);
+}
+
 var out = require( "fs" ).createWriteStream( "result.zip" );
 new Builder( components ).writeTo( out, function( error, result ) {
 	if ( error ) {
