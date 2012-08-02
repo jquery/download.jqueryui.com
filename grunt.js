b7fb919065541857cd5b4b5284717dd452387436
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-08-02 12:37:59 +0200

    No top-level indent

--- grunt.js
+++ grunt.js
@@ -1,32 +1,32 @@
 module.exports = function(grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		pkg: "<json:package.json>",
-		lint: {
-			files: [ "*.js", "app/**/*.js" ]
-		},
-		jshint: {
-			options: {
-				curly: true,
-				eqeqeq: true,
-				immed: true,
-				latedef: true,
-				newcap: true,
-				noarg: true,
-				sub: true,
-				undef: true,
-				boss: true,
-				eqnull: true,
-				node: true,
-				smarttabs: true,
-				trailing: true,
-				onevar: true
-			}
+// Project configuration.
+grunt.initConfig({
+	pkg: "<json:package.json>",
+	lint: {
+		files: [ "*.js", "app/**/*.js" ]
+	},
+	jshint: {
+		options: {
+			curly: true,
+			eqeqeq: true,
+			immed: true,
+			latedef: true,
+			newcap: true,
+			noarg: true,
+			sub: true,
+			undef: true,
+			boss: true,
+			eqnull: true,
+			node: true,
+			smarttabs: true,
+			trailing: true,
+			onevar: true
 		}
-	});
+	}
+});
 
-	// Default task.
-	grunt.registerTask( "default", "lint" );
+// Default task.
+grunt.registerTask( "default", "lint" );
 
 };
