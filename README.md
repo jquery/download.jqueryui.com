356bcd22fb59a3e6bf6ffe10a10221005ac11fd4
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-08-02 14:10:11 +0200

    Grunt task 'prepare' to generate distribution files

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 ## jQuery UI Download Builder
 
-Run a jQuery UI build with `grunt release`, then copy over the result, e.g.
-`dist/jquery-ui-1.9` into the `versions` dir into this project.
-
-Also run the `grunt manifest` task (from manifest branch if its not yet in
-master) and move the *.jquery.json files into that same folder.
-
 `npm install` to install node modules.
+
+`grunt prepare:[tag/branch]` to create distribution files, e.g. `grunt prepare:1.9.0` or `grunt prepare:master`
 
 `node cli-test.js` to run the backend, outputs `result.zip`
 
 `node build-server.js` to run the server, go to the URL it outputs.
 
-Add `--port 3000` or `--host ...` to specify custom host and port.
+Add `--port 3000` or `--host ...` to specify custom host and port, e.g. `node build-server.js --port 3000`
--- grunt.js
+++ grunt.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
 
-// Project configuration.
 grunt.initConfig({
 	pkg: "<json:package.json>",
 	lint: {
@@ -26,7 +25,107 @@ grunt.initConfig({
 	}
 });
 
-// Default task.
 grunt.registerTask( "default", "lint" );
+
+function cloneOrFetch( callback ) {
+	var fs = require( "fs" );
+	if ( fs.existsSync( "jquery-ui" ) ) {
+		grunt.log.writeln( "Fetch updates for jquery-ui repo" );
+		grunt.utils.spawn({
+			cmd: "git",
+			args: [ "fetch" ],
+			opts: {
+				cwd: "jquery-ui"
+			}
+		}, function() {
+			grunt.log.ok( "Fetched repo" );
+			callback();
+		});
+	} else {
+		grunt.log.writeln( "Cloning jquery-ui repo" );
+		grunt.utils.spawn({
+			cmd: "git",
+			args: [ "clone", "git://github.com/jquery/jquery-ui.git", "jquery-ui" ]
+		}, function() {
+			grunt.log.ok( "Cloned repo" );
+			callback();
+		});
+	}
+}
+
+function checkout( branchOrTag, callback ) {
+	grunt.log.writeln( "Checking out branch/tag: " + branchOrTag );
+	grunt.utils.spawn({
+		cmd: "git",
+		args: [ "checkout", "-f", "origin/" + branchOrTag ],
+		opts: {
+			cwd: "jquery-ui"
+		}
+	}, function() {
+		grunt.log.ok( "Done with checkout" );
+		callback();
+	});
+}
+
+function install( callback ) {
+	grunt.log.writeln( "Installing npm modules" );
+	grunt.utils.spawn({
+		cmd: "npm",
+		args: [ "install" ],
+		opts: {
+			cwd: "jquery-ui"
+		}
+	}, function() {
+		grunt.log.ok( "Installed npm modules" );
+		callback();
+	});
+}
+
+function build( callback ) {
+	grunt.log.writeln( "Building jQuery UI" );
+	grunt.utils.spawn({
+		cmd: "grunt",
+		args: [ "manifest" ],
+		opts: {
+			cwd: "jquery-ui"
+		}
+	}, function() {
+		grunt.utils.spawn({
+			cmd: "grunt",
+			args: [ "release" ],
+			opts: {
+				cwd: "jquery-ui"
+			}
+		}, function() {
+			grunt.log.ok( "Done building" );
+			callback();
+		});
+	});
+}
+
+function copy( callback ) {
+	var dir = require( "path" ).basename( grunt.file.expandDirs( "jquery-ui/dist/jquery-ui-*" )[ 0 ] );
+	grunt.utils.spawn({
+		cmd: "cp",
+		args: [ "-r", "jquery-ui/dist/" + dir, "versions/" + dir ]
+	}, function() {
+		grunt.log.ok( "Copied release over to versions/" + dir );
+		callback();
+	});
+}
+
+grunt.registerTask( "prepare", "Fetches jQuery UI and builds the specified branch or tag", function( branchOrTag ) {
+	var done = this.async();
+	grunt.file.mkdir( "versions" );
+	cloneOrFetch(function( error, output ) {
+		checkout( branchOrTag, function( error, output ) {
+			install(function() {
+				build(function() {
+					copy( done );
+				});
+			});
+		});
+	});
+});
 
 };
