5cbe6466e274665a4fbf8c9b656c0694a5394ad0
Rafael Xavier de Souza <rxaviers@gmail.com>
2012-08-18 09:11:50 -0300

    Port banner-test.js to a unit test

---
+++ test/banner.js
@@ -0,0 +1,35 @@
+var banner = require( "../lib/banner.js" ),
+  dateformat = require( "dateformat" );
+
+function today(format) {
+	return dateformat(new Date(), format);
+}
+
+var pkg = {
+		title: "jQuery UI",
+		version: "1.9.0",
+		homepage: "http://jqueryui.com",
+		author: {
+			name: "authors.txt"
+		},
+		licenses: [
+			{
+				type: "MIT"
+			},
+			{
+				type: "GPL"
+			}
+		]
+	},
+	fileNames = [ "jquery.ui.autocomplete.js", "jquery.ui.accordion.js" ],
+	output = "/*! jQuery UI - v1.9.0 - " + today("isoDate") + "\n" +
+		"* http://jqueryui.com\n" +
+		"* Includes: jquery.ui.autocomplete.js, jquery.ui.accordion.js\n" +
+		"* Copyright (c) 2012 authors.txt Licensed MIT, GPL */";
+
+module.exports = {
+	"test: case 1": function( test ) {
+		test.ok( output == banner( pkg, fileNames ), "Not expected output" );
+		test.done();
+	}
+};
