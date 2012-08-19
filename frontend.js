1a270cd437f8aa15952a249474b75a6625f2c770
Rafael Xavier de Souza <rxaviers@gmail.com>
2012-08-19 10:48:49 -0300

    Add version section in the frontend

--- frontend.js
+++ frontend.js
@@ -2,12 +2,13 @@ var fs = require( "fs" ),
 	handlebars = require( "handlebars" ),
 	homeTemplate = handlebars.compile( fs.readFileSync( __dirname + "/template/frontend_body.html", "utf8" ) ),
 	wrapperTemplate = handlebars.compile( fs.readFileSync( __dirname + "/template/frontend.html", "utf8" ) ),
-	categories = require( "./lib/release" ).all()[ 0 ].categories();
+	release = require( "./lib/release" ).all()[ 0 ];
 
 module.exports = function( action ) {
 	return homeTemplate( {
 		action: action || "/download",
-		categories: categories
+		categories: release.categories(),
+		pkg: release.pkg
 	});
 };
 module.exports.wrapped = function() {
