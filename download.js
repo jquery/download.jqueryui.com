45e8cc67648f7ce8accec03ca8e69daabdb45233
Rafael Xavier de Souza <rxaviers@gmail.com>
2012-09-18 15:55:46 -0300

    Fix indentation tabs

--- download.js
+++ download.js
@@ -10,49 +10,49 @@ Handlebars.registerHelper( "isSelectedTheme", function( theme, selectedTheme ) {
 	return theme.isEqual( selectedTheme ) ? "selected=\"selected\"" : "";
 });
 
-var bodyTemplate = Handlebars.compile( fs.readFileSync( __dirname + "/template/download/body.html", "utf8" ) ),
+var bodyTemplate = Handlebars.compile( fs.readFileSync( __dirname + "/template/download/index.html", "utf8" ) ),
 	indexTemplate = Handlebars.compile( fs.readFileSync( __dirname + "/template/download/index.html", "utf8" ) );
 
 var Frontend = function( host ) {
-  this.host = host;
+	this.host = host;
 };
 
 Frontend.prototype = {
-  index: function( params ) {
-    var customTheme;
-    var selectedTheme = themeGallery[ 0 ];
-    // TODO these needs to be handled on the client side
-    if ( params && params.themeParams ) {
-      customTheme = new ThemeRoller( deserialize( "?" + unescape( params.themeParams ) ) );
-      var inThemeGallery = themeGallery.some(function( theme ) {
-          var isEqual = customTheme.isEqual( theme );
-          if ( isEqual ) {
-            selectedTheme = theme;
-          }
-          return isEqual;
-        });
-      if ( !inThemeGallery ) {
-        customTheme.name = "Custom Theme";
-        selectedTheme = customTheme;
-      } else {
-        customTheme = false;
-      }
-    }
-    return bodyTemplate( {
-      host: this.host,
-      categories: release.categories(),
-      folderName: selectedTheme.folderName(),
-      pkg: release.pkg,
-      selectedTheme: selectedTheme,
-      themeGallery: customTheme ?  [ customTheme ].concat( themeGallery ) : themeGallery
-    });
-  },
+	index: function( params ) {
+		var customTheme;
+		var selectedTheme = themeGallery[ 0 ];
+		// TODO these needs to be handled on the client side
+		if ( params && params.themeParams ) {
+			customTheme = new ThemeRoller( deserialize( "?" + unescape( params.themeParams ) ) );
+			var inThemeGallery = themeGallery.some(function( theme ) {
+					var isEqual = customTheme.isEqual( theme );
+					if ( isEqual ) {
+						selectedTheme = theme;
+					}
+					return isEqual;
+				});
+			if ( !inThemeGallery ) {
+				customTheme.name = "Custom Theme";
+				selectedTheme = customTheme;
+			} else {
+				customTheme = false;
+			}
+		}
+		return bodyTemplate( {
+			host: this.host,
+			categories: release.categories(),
+			folderName: selectedTheme.folderName(),
+			pkg: release.pkg,
+			selectedTheme: selectedTheme,
+			themeGallery: customTheme ?  [ customTheme ].concat( themeGallery ) : themeGallery
+		});
+	},
 
-  download: function( params ) {
-    return indexTemplate({
-      body: module.exports( "/download", params )
-    });
-  }
+	download: function( params ) {
+		return indexTemplate({
+			body: module.exports( "/download", params )
+		});
+	}
 };
 
 module.exports = Frontend;
