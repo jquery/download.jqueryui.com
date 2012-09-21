f7d654d58210731dd89accdcc2a55ed9225544b3
Rafael Xavier de Souza <rxaviers@gmail.com>
2012-09-21 10:24:33 -0300

    Double quoting strings

--- app/resources/themeroller.js
+++ app/resources/themeroller.js
@@ -11,7 +11,7 @@
 	 */
 
 	//global - TR app or bookmarklet
-	var bookmarklet = window.location.href.indexOf( 'developertool' ) > -1;
+	var bookmarklet = window.location.href.indexOf( "developertool" ) > -1;
 
 	//global for tracking open and focused toolbar panels on refresh
 	var openGroups = [];
@@ -19,9 +19,9 @@
 
 	//backbutton and hash bookmarks support
 	var hash = {
-		storedHash: '',
-		currentTabHash: '', //The hash that's only stored on a tab switch
-		cache: '',
+		storedHash: "",
+		currentTabHash: "", //The hash that"s only stored on a tab switch
+		cache: "",
 		interval: null,
 		listen: true, // listen to hash changes?
 
@@ -64,22 +64,22 @@
 
 			// remember which groups are open
 			openGroups = [];
-			$( 'div.theme-group-content' ).each(function( i ) {
-				if ( $( this ).is( ':visible' ) ) {
+			$( "div.theme-group-content" ).each(function( i ) {
+				if ( $( this ).is( ":visible" ) ) {
 					openGroups.push( i );
 				}
 			});
 
 			// remember any focused element
 			focusedEl = null;
-			$( 'form input, form select, form .texturePicker' ).each(function( i ) {
-				if ( $( this ).is( '.focus' ) ) {
+			$( "form input, form select, form .texturePicker" ).each(function( i ) {
+				if ( $( this ).is( ".focus" ) ) {
 					focusedEl = i;
 				}
 			});
 
 			// reload tab
-			$.ajax( downloadJqueryuiHost + '/themeroller/rollyourown?themeParams=' + escape( locStr ), {
+			$.ajax( downloadJqueryuiHost + "/themeroller/rollyourown?themeParams=" + escape( locStr ), {
 				dataType: "jsonp",
 				success: function( response ) {
 					$( "#rollYourOwn" ).html( response );
@@ -123,14 +123,14 @@
 		},
 
 		init: function(){
-			hash.storedHash = '';
+			hash.storedHash = "";
 			hash.checkHashChange();
 		}
 	};
 
 	//function to append a new theme stylesheet with the new style changes
 	function updateCSS( locStr ){
-		$( "body" ).append( '<link href="' + downloadJqueryuiHost + '/themeroller/parsetheme.css' + ( locStr ? "?" + locStr : "" ) + '" type="text/css" rel="Stylesheet" />');
+		$( "body" ).append( "<link href=\"" + downloadJqueryuiHost + "/themeroller/parsetheme.css" + ( locStr ? "?" + locStr : "" ) + "\" type=\"text/css\" rel=\"Stylesheet\" />");
 		var links = $( "link[href*=parsetheme\\.css]" );
 		if ( links.length > 1 ) {
 			// wait a few seconds before removing previous theme(s) to avoid FOUW
@@ -142,7 +142,7 @@
 
 	//function called after a change event in the form
 	function formChange(){
-		var locStr = $( '.themeroller .application form' ).serialize();
+		var locStr = $( ".themeroller .application form" ).serialize();
 		locStr = hash.clean( locStr );
 		updateCSS( locStr );
 		hash.updateHash( locStr, true );
@@ -154,12 +154,12 @@
 			var $this = $( this );
 
 			$this.next().slideToggle( 100 );
-			$this.find( '.arrow-icon' ).toggleClass( 'icon-triangle-1-s' ).end().toggleClass( 'state-active' );
+			$this.find( ".arrow-icon" ).toggleClass( "icon-triangle-1-s" ).end().toggleClass( "state-active" );
 
-			if ( $this.is( '.corner-all' ) ) {
-				$this.removeClass( 'corner-all' ).addClass( 'corner-top' );
-			} else if ( $this.is( '.corner-top' ) ) {
-				$this.removeClass( 'corner-top' ).addClass( 'corner-all' );
+			if ( $this.is( ".corner-all" ) ) {
+				$this.removeClass( "corner-all" ).addClass( "corner-top" );
+			} else if ( $this.is( ".corner-top" ) ) {
+				$this.removeClass( "corner-top" ).addClass( "corner-all" );
 			}
 			return false;
 		});
@@ -169,13 +169,13 @@
 	$.fn.validHex = function() {
 		return this.each(function() {
 			var value = $( this ).val();
-			value = value.replace( /[^#a-fA-F0-9]/g, '' ); // non [#a-f0-9]
+			value = value.replace( /[^#a-fA-F0-9]/g, "" ); // non [#a-f0-9]
 			value = value.toLowerCase();
 			if ( value.match( /#/g ) && value.match( /#/g ).length > 1 ) {
-				value = value.replace( /#/g, '' ); // ##
+				value = value.replace( /#/g, "" ); // ##
 			}
-			if ( value.indexOf( '#' ) == -1 ) {
-				value = '#'+value; // no #
+			if ( value.indexOf( "#" ) == -1 ) {
+				value = "#"+value; // no #
 			}
 			if ( value.length > 7 ) {
 				value = value.substr( 0, 7 ); // too many chars
@@ -187,68 +187,68 @@
 	//color pickers setup (sets bg color of inputs)
 	$.fn.applyFarbtastic = function() {
 		return this.each(function() {
-			$( '<div/>' ).farbtastic( this ).remove();
+			$( "<div/>" ).farbtastic( this ).remove();
 		});
 	};
 
 
 
 
-	// events within the 'roll your own' tab
+	// events within the "roll your own" tab
 	function rollYourOwnBehaviors() {
 
 		// hover class toggles in app panel
-		$( 'li.state-default, div.state-default' ).hover(
-			function(){ $( this ).addClass( 'state-hover' ); },
-			function(){ $( this ).removeClass( 'state-hover' ); }
+		$( "li.state-default, div.state-default" ).hover(
+			function(){ $( this ).addClass( "state-hover" ); },
+			function(){ $( this ).removeClass( "state-hover" ); }
 		);
 
 		// hex inputs
-		$( 'input.hex' )
+		$( "input.hex" )
 			.validHex()
 			.keyup(function() {
 				$( this ).validHex();
 			})
 			.click(function(){
-				$( this ).addClass( 'focus' );
-				$( '#picker' ).remove();
-				$( 'div.picker-on' ).removeClass( 'picker-on' );
-				$( 'div.texturePicker ul:visible' ).hide( 0 ).parent().css( 'position', 'static' );
-				$( this ).after( '<div id="picker"></div>' ).parent().addClass( 'picker-on' );
-				$( '#picker' ).farbtastic( this );
+				$( this ).addClass( "focus" );
+				$( "#picker" ).remove();
+				$( "div.picker-on" ).removeClass( "picker-on" );
+				$( "div.texturePicker ul:visible" ).hide( 0 ).parent().css( "position", "static" );
+				$( this ).after( "<div id=\"picker\"></div>" ).parent().addClass( "picker-on" );
+				$( "#picker" ).farbtastic( this );
 				return false;
 			})
-			.wrap( '<div class="hasPicker"></div>' )
+			.wrap( "<div class=\"hasPicker\"></div>" )
 			.applyFarbtastic();
 
 		// focus and blur classes in form
-		$( 'input, select' )
+		$( "input, select" )
 		.focus(function() {
-			$( 'input.focus, select.focus' ).removeClass( 'focus' );
-			$( this ).addClass( 'focus' );
+			$( "input.focus, select.focus" ).removeClass( "focus" );
+			$( this ).addClass( "focus" );
 		})
 		.blur(function() {
-			$( this ).removeClass( 'focus' );
+			$( this ).removeClass( "focus" );
 		});
 
 		// texture pickers from select menus
-		$( 'select.texture' ).each(function() {
+		$( "select.texture" ).each(function() {
 
-			$( this ).after( '<div class="texturePicker"><a href="#"></a><ul></ul></div>' );
+			$( this ).after( "<div class=\"texturePicker\"><a href=\"#\"></a><ul></ul></div>" );
 			var texturePicker = $( this ).next();
-			var a = texturePicker.find( 'a' );
-			var ul = texturePicker.find( 'ul' );
+			var a = texturePicker.find( "a" );
+			var ul = texturePicker.find( "ul" );
 			var sIndex = texturePicker.prev().get( 0 ).selectedIndex;
 
 			// scrape options
-			$( this ).find( 'option' ).each(function(){
-				ul.append( '<li class="'+ $( this ).attr( 'value' ) +'" data-texturewidth="'+$( this ).attr( 'data-texturewidth' )+'" data-textureheight="'+$( this ).attr( 'data-texturewidth' )+'" style="background: #555555 url( http://jqueryui.com/themeroller/images/?new=555555&w='+$( this ).attr( 'data-texturewidth' )+'&h='+$( this ).attr( 'data-textureheight' )+'&f=png&q=100&fltr[]=over|textures/'+$( this ).attr( 'value' )+'|0|0|100 ) 50% 50% repeat"><a href="#" title="'+ $( this ).text() +'">'+ $( this ).text() +'</a></li>' );
-				if( $( this ).get( 0 ).index == sIndex ){texturePicker.attr( 'title',$( this ).text() ).css( 'background', '#555555 url( http://jqueryui.com/themeroller/images/?new=555555&w='+$( this ).attr( 'data-texturewidth' )+'&h='+$( this ).attr( 'data-textureheight' )+'&f=png&q=60&fltr[]=over|textures/'+$( this ).attr( 'value' )+'|0|0|100 ) 50% 50% repeat' );}
+			$( this ).find( "option" ).each(function(){
+				ul.append( "<li class=\""+ $( this ).attr( "value" ) +"\" data-texturewidth=\""+$( this ).attr( "data-texturewidth" )+"\" data-textureheight=\""+$( this ).attr( "data-texturewidth" )+"\" style=\"background: #555555 url( http://jqueryui.com/themeroller/images/?new=555555&w="+$( this ).attr( "data-texturewidth" )+"&h="+$( this ).attr( "data-textureheight" )+"&f=png&q=100&fltr[]=over|textures/"+$( this ).attr( "value" )+"|0|0|100 ) 50% 50% repeat\"><a href=\"#\" title=\""+ $( this ).text() +"\">"+ $( this ).text() +"</a></li>" );
+				if( $( this ).get( 0 ).index == sIndex ){texturePicker.attr( "title",$( this ).text() ).css( "background", "#555555 url( http://jqueryui.com/themeroller/images/?new=555555&w="+$( this ).attr( "data-texturewidth" )+"&h="+$( this ).attr( "data-textureheight" )+"&f=png&q=60&fltr[]=over|textures/"+$( this ).attr( "value" )+"|0|0|100 ) 50% 50% repeat" );}
 			} );
 
-			ul.find( 'li' ).click(function() {
-				texturePicker.prev().get( 0 ).selectedIndex = texturePicker.prev().find( 'option[value='+ $( this ).attr( 'class' ).replace( /\./g, '\\.' ) +']' ).get( 0 ).index;
-				texturePicker.attr( 'title',$( this ).text() ).css( 'background', '#555555 url( http://jqueryui.com/themeroller/images/?new=555555&w='+$( this ).attr( 'data-texturewidth' )+'&h='+$( this ).attr( 'data-textureheight' )+'&f=png&q=60&fltr[]=over|textures/'+$( this ).attr( 'class' )+'|0|0|100 )  50% 50% repeat' );
+			ul.find( "li" ).click(function() {
+				texturePicker.prev().get( 0 ).selectedIndex = texturePicker.prev().find( "option[value="+ $( this ).attr( "class" ).replace( /\./g, "\\." ) +"]" ).get( 0 ).index;
+				texturePicker.attr( "title",$( this ).text() ).css( "background", "#555555 url( http://jqueryui.com/themeroller/images/?new=555555&w="+$( this ).attr( "data-texturewidth" )+"&h="+$( this ).attr( "data-textureheight" )+"&f=png&q=60&fltr[]=over|textures/"+$( this ).attr( "class" )+"|0|0|100 )  50% 50% repeat" );
 				//ul.fadeOut( 100 );
 				formChange();
 				return false;
@@ -259,15 +259,15 @@
 
 			// show/hide of menus
 			texturePicker.click(function() {
-				$( this ).addClass( 'focus' );
-				$( '#picker' ).remove();
+				$( this ).addClass( "focus" );
+				$( "#picker" ).remove();
 				var showIt;
-				if ( ul.is( ':hidden' ) ) {
+				if ( ul.is( ":hidden" ) ) {
 					showIt = true;
 				}
-				$( 'div.texturePicker ul:visible' ).hide().parent().css( 'position', 'static' );
+				$( "div.texturePicker ul:visible" ).hide().parent().css( "position", "static" );
 				if ( showIt === true ) {
-					texturePicker.css( 'position', 'relative' );
+					texturePicker.css( "position", "relative" );
 					ul.show();
 				}
 
@@ -276,27 +276,27 @@
 		});
 
 		// spindowns in TR panel
-		$( 'div.theme-group .theme-group-header' ).addClass( 'corner-all' ).spinDown();
+		$( "div.theme-group .theme-group-header" ).addClass( "corner-all" ).spinDown();
 
 		// change event in form
-		$( '.themeroller .application form' ).bind( 'change', function() {
+		$( ".themeroller .application form" ).bind( "change", function() {
 			formChange();
 			return false;
 		} );
 
 		// return key triggers form change
-		$.hotkeys.add( 'return', function () { formChange(); } );
+		$.hotkeys.add( "return", function () { formChange(); } );
 
 		//DL theme button
-		$( '#downloadTheme' ).click(function(){
-			// FIXME shouldn't we refresh/update the href on form.change?
+		$( "#downloadTheme" ).click(function(){
+			// FIXME shouldn"t we refresh/update the href on form.change?
 			// Issue: if user right clicks to copy its url, he will copy a wrong/outdated link.
-			var href = $( 'link[href*=parsetheme\\.css]:last' ).attr( 'href' );
-			href = href.replace( '','' );
-			var themeParams = /\?/.test( href ) ? escape( href.split( '?' )[ 1 ] ) : null;
-			var themeParamName = ( bookmarklet ) ? 'theme' : 'themeParams';
-			var straightToDownload = ( bookmarklet ) ? '&ui-version=1.7&download=true' : '';
-			location.href = '/download' + ( themeParams ? '?' + themeParamName + '=' + themeParams + straightToDownload : '' );
+			var href = $( "link[href*=parsetheme\\.css]:last" ).attr( "href" );
+			href = href.replace( "","" );
+			var themeParams = /\?/.test( href ) ? escape( href.split( "?" )[ 1 ] ) : null;
+			var themeParamName = ( bookmarklet ) ? "theme" : "themeParams";
+			var straightToDownload = ( bookmarklet ) ? "&ui-version=1.7&download=true" : "";
+			location.href = "/download" + ( themeParams ? "?" + themeParamName + "=" + themeParams + straightToDownload : "" );
 			return false;
 		} );
 
@@ -307,24 +307,24 @@
 	function themeGalleryBehaviors() {
 
 		// loading and viewing gallery themes
-		$( '#themeGallery a' )
-			.bind( 'click', function() {
-				updateCSS( hash.clean( this.href.split( '?' )[ 1 ] ));
-				hash.updateHash( hash.clean( this.href.split( '?' )[ 1 ] ), true );
+		$( "#themeGallery a" )
+			.bind( "click", function() {
+				updateCSS( hash.clean( this.href.split( "?" )[ 1 ] ));
+				hash.updateHash( hash.clean( this.href.split( "?" )[ 1 ] ), true );
 				return false;
 			} )
-			.attr( 'title', 'Click to preview this theme' )
+			.attr( "title", "Click to preview this theme" )
 			.each(function(){
-				var straightToDownload = ( bookmarklet ) ? '&ui-version=1.7&download=true' : '';
+				var straightToDownload = ( bookmarklet ) ? "&ui-version=1.7&download=true" : "";
 				$( this ).after(
-				'<a href="/download?themeParams=' + escape( $( this ).attr( 'href' ).split( '?' )[ 1 ] ) + straightToDownload + '" class="download" title="Download this theme">Download</a>'+
-				'<a href="#" class="edit" title="Customize this theme">Edit</a>' );
+				"<a href=\"/download?themeParams=" + escape( $( this ).attr( "href" ).split( "?" )[ 1 ] ) + straightToDownload + "\" class=\"download\" title=\"Download this theme\">Download</a>"+
+				"<a href=\"#\" class=\"edit\" title=\"Customize this theme\">Edit</a>" );
 			})
 			.parent()
-			.find( 'a.edit' )
+			.find( "a.edit" )
 			.click(function() {
-				$( this ).prev().prev().trigger( 'click' );
-				$( '#rollerTabs' ).tabs( "select", 0 );
+				$( this ).prev().prev().trigger( "click" );
+				$( "#rollerTabs" ).tabs( "select", 0 );
 				return false;
 			});
 
@@ -333,10 +333,10 @@
 	function initRollOver() {
 		rollYourOwnBehaviors();
 		if( openGroups.length > 0 ){
-			openGroups.join( ',' ); $( '.theme-group-content:eq( '+openGroups+' )' ).prev().trigger( 'click' );
+			openGroups.join( "," ); $( ".theme-group-content:eq( "+openGroups+" )" ).prev().trigger( "click" );
 		}
 		if( focusedEl ){
-			$( 'form input, form select, form .texturePicker' ).eq( focusedEl ).click();
+			$( "form input, form select, form .texturePicker" ).eq( focusedEl ).click();
 		}
 		openGroups = [];
 		focusedEl = null;
@@ -347,13 +347,13 @@
 	$(function() {
 
 		//app tabs
-		$( '#rollerTabs' ).tabs( {
+		$( "#rollerTabs" ).tabs( {
 			load: function( e, ui ){
 				initRollOver();
 			},
-			spinner: 'Loading...',
+			spinner: "Loading...",
 			select: function( e,ui ){
-				if( $( ui.panel ).is( '#rollYourOwn' )  && hash.currHash() != hash.currentTabHash ) { // Stop if we actually don't have a hash change
+				if( $( ui.panel ).is( "#rollYourOwn" )  && hash.currHash() != hash.currentTabHash ) { // Stop if we actually don"t have a hash change
 					hash.refreshToHash();
 				}
 				hash.currentTabHash = hash.currHash();
@@ -367,22 +367,22 @@
 		themeGalleryBehaviors();
 
 		//general app click cleanup
-		$( 'body' ).click(function() {
-			$( 'div.picker-on' ).removeClass( 'picker-on' );
-			$( '#picker' ).remove();
-			$( 'input.focus, select.focus' ).removeClass( 'focus' );
-			$( 'div.texturePicker ul:visible' ).hide().parent().css( 'position', 'static' );
+		$( "body" ).click(function() {
+			$( "div.picker-on" ).removeClass( "picker-on" );
+			$( "#picker" ).remove();
+			$( "input.focus, select.focus" ).removeClass( "focus" );
+			$( "div.texturePicker ul:visible" ).hide().parent().css( "position", "static" );
 		});
 
 		//links to roll your own from help tab
-		$( '#help a[href="#rollYourOwn"]' ).click(function() {
-			$( '#rollerTabs' ).tabs( "select", 0 );
+		$( "#help a[href=\"#rollYourOwn\"]" ).click(function() {
+			$( "#rollerTabs" ).tabs( "select", 0 );
 			return false;
 		});
 
 		//links to theme gallery from help tab
-		$( '#help a[href="#themeGallery"]' ).click(function() {
-			$( '#rollerTabs' ).tabs( "select", 1 );
+		$( "#help a[href=\"#themeGallery\"]" ).click(function() {
+			$( "#rollerTabs" ).tabs( "select", 1 );
 			return false;
 		});
 
