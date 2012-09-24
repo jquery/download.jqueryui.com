jquery/download.jqueryui.com

commit e65d7e2d49a40c8f01e59c40355b68e6767ef828

Replace custom serialize/deserialize with node's querystring

lib/themeroller.js
@@ -2,8 +2,8 @@ var _ = require( "underscore" ),
 	async = require( "async" ),
 	fs = require( "fs" ),
 	http = require( "http" ),
-  serialize = require( "./util" ).serialize,
 	path = require( "path" ),
+	querystring = require( "querystring" ),
 	release = require( "./release" ).all()[ 0 ],
 	textures = require( "./themeroller.textures.js" );
 
@@ -78,7 +78,7 @@ function ThemeRoller( vars ) {
 		delete vars.name;
 		delete vars.scope;
 	}
-  this.serializedVars = serialize( vars );
+  this.serializedVars = querystring.stringify( vars );
 	vars = this.vars = _.extend( {}, ThemeRoller.defaults, vars );
 	this.images = {};
 
@@ -174,7 +174,7 @@ ThemeRoller.prototype = {
 
 	_iconUrl: function( color ) {
 		var filename = "ui-icons_" + color + "_256x240.png";
-		this._setImage( filename, "?" + serialize( [ [ "new", color ], [ "w", iconDimension[0] ], [ "h", iconDimension[1] ], [ "f", "png" ], [ "fltr[]", "rcd|256" ], [ "fltr[]", "mask|icons/icons.png" ] ] ) );
+		this._setImage( filename, "?" + querystring.stringify( { "new": color, "w": iconDimension[0], "h": iconDimension[1], "f": "png", "fltr[]": "rcd|256", "fltr[]": "mask|icons/icons.png" } ) );
 		return this._imageUrl( filename );
 	},
 
@@ -192,7 +192,7 @@ ThemeRoller.prototype = {
 		}
 		var dimension = textureDimensions[ file ],
 			filename = "ui-bg_" + file.replace( /[0-9]*_([^\.]*).png/, "$1" ).replace( /_/g, "-" ) + "_" + opacity + "_" + color + "_" + dimension.join( "x" ) + ".png";
-		this._setImage( filename, "?" + serialize( [ [ "new", color], [ "w", dimension[0] ], [ "h", dimension[1] ], [ "f", "png"] , [ "q", "100" ], [ "fltr[]", "over|textures/" + file + "|0|0|" + opacity ] ] ) );
+		this._setImage( filename, "?" + querystring.stringify( { "new": color, "w": dimension[0], "h": dimension[1], "f": "png", "q": "100", "fltr[]": "over|textures/" + file + "|0|0|" + opacity } ) );
 		 
 		return this._imageUrl( filename );
 	},

themeroller.js
@@ -1,7 +1,7 @@
 var _ = require( "underscore" ),
-	deserialize = require( "./lib/util" ).deserialize,
 	fs = require( "fs" ),
 	Handlebars = require( "handlebars" ),
+	querystring = require( "querystring" ),
 	textures = require( "./lib/themeroller.textures" ),
 	themeGallery = require( "./lib/themeroller.themegallery" ),
 	ThemeRoller = require( "./lib/themeroller" );
@@ -82,7 +82,7 @@ Frontend.prototype = {
 	},
 
 	rollYourOwn: function( params ) {
-		var theme = new ThemeRoller( deserialize( "?" + unescape( params.themeParams ) ) );
+		var theme = new ThemeRoller( querystring.parse( unescape( params.themeParams ) ) );
 		return jsonpTemplate({
 				callback: params.callback,
 				data: JSON.stringify( rollyourownTemplate( theme ) )

download.js
@@ -1,7 +1,7 @@
 var _ = require( "underscore" ),
-	deserialize = require( "./lib/util" ).deserialize,
 	fs = require( "fs" ),
 	Handlebars = require( "handlebars" ),
+	querystring = require( "querystring" ),
 	release = require( "./lib/release" ).all()[ 0 ],
 	themeGallery = require( "./lib/themeroller.themegallery" ),
 	ThemeRoller = require( "./lib/themeroller" );
@@ -39,7 +39,7 @@ Frontend.prototype = {
 	theme: function( params ) {
 		var selectedTheme = themeGallery[ 0 ];
 		if ( params.themeParams ) {
-			selectedTheme = new ThemeRoller( deserialize( "?" + unescape( params.themeParams ) ) );
+			selectedTheme = new ThemeRoller( querystring.parse( unescape( params.themeParams ) ) );
 		}
 		return jsonpTemplate({
 			callback: params.callback,

lib/util.js
@@ -1,24 +1,0 @@
-var Url = require( "url" );
-
-module.exports = {
-	// FIXME Shall we use an external lib for this?
-	serialize: function param( params ) {
-		if ( typeof params == "undefined" ) {
-			return "";
-		}
-		if ( typeof params == "object" && !Array.isArray( params ) ) {
-			params = Object.keys( params ).map(function( k ) {
-				return [ [ k ], [ params[k] ] ];
-			}); 
-		}
-		return params.map(function( param ) {
-			return encodeURIComponent( param[0] ) + "=" + encodeURIComponent( param[1] );
-		}).join( "&" ).replace( /%20/g, "+" );
-	},
-	
-	// FIXME Shall we use an external lib for this?
-	deserialize: function( url ) {
-		var urlParts = Url.parse( url, true );
-		return urlParts.query;
-	}
-};

server.js
@@ -9,7 +9,7 @@ process.on( "uncaughtException", function ( err ) {
 var argv = require( "optimist" ).argv,
 	Builder = require( "./lib/builder" ),
 	connect = require( "connect" ),
-	deserialize = require( "./lib/util" ).deserialize,
+	querystring = require( "querystring" ),
 	formidable = require( "formidable" ),
 	Frontend = require( "./frontend" ),
 	httpHost = argv.host || "localhost",
@@ -27,12 +27,16 @@ var argv = require( "optimist" ).argv,
 
 var frontend = new Frontend();
 
+function params( request ) {
+	return querystring.parse( request.url.split( "?" )[ 1 ] );
+}
+
 function route(app) {
 	app.get( routes.home, function( request, response, next ) {
 		response.end( frontend.root() );
 	});
 	app.get( routes.download, function( request, response, next) {
-		response.end( frontend.download.index( deserialize( request.url ), {
+		response.end( frontend.download.index( params( request ), {
 			wrap: true
 		}));
 	});
@@ -41,7 +45,7 @@ function route(app) {
 		form.parse( request, function( err, fields, files ) {
 			var field, builder, themeVars,
 				components = [];
-			themeVars = fields.theme == "none" ? null : deserialize( "?" + fields.theme );
+			themeVars = fields.theme == "none" ? null : querystring.parse( fields.theme );
 			if ( themeVars !== null && fields.themeFolderName ) {
 				themeVars.folderName = fields.themeFolderName;
 			}
@@ -65,20 +69,20 @@ function route(app) {
 	});
 	app.get( routes.downloadTheme, function( request, response, next ) {
 		response.setHeader( "Content-Type", "application/json" );
-		response.end( frontend.download.theme( deserialize( request.url ) ) );
+		response.end( frontend.download.theme( params( request ) ) );
 	});
 	app.get( routes.themeroller, function( request, response, next ) {
-		response.end( frontend.themeroller.index( deserialize( request.url ), {
+		response.end( frontend.themeroller.index( params( request ), {
 			wrap: true
 		}));
 	});
 	app.get( routes.themerollerParseTheme, function( request, response, next ) {
 		response.setHeader( "Content-Type", "text/css" );
-		response.end( frontend.themeroller.css( deserialize( request.url ) ) );
+		response.end( frontend.themeroller.css( params( request ) ) );
 	});
 	app.get( routes.themerollerRollYourOwn, function( request, response, next ) {
 		response.setHeader( "Content-Type", "application/json" );
-		response.end( frontend.themeroller.rollYourOwn( deserialize( request.url ) ) );
+		response.end( frontend.themeroller.rollYourOwn( params( request ) ) );
 	});
 }
 

commit 17c3c00238d9eeade1f9b76be32b112624e0a32d

Fix querystring.stringify parameter "fltr[]"
- regression bug introduced by e65d7e2d49a40c8f01e59c40355b68e6767ef828

lib/themeroller.js
@@ -186,7 +186,7 @@ ThemeRoller.prototype = {
 		var filename;
 		color = expandColor( color );
 		filename = "ui-icons_" + color + "_256x240.png";
-		this._setImage( filename, "?" + querystring.stringify( { "new": color, "w": iconDimension[0], "h": iconDimension[1], "f": "png", "fltr[]": "rcd|256", "fltr[]": "mask|icons/icons.png" } ) );
+		this._setImage( filename, "?" + querystring.stringify( { "new": color, "w": iconDimension[0], "h": iconDimension[1], "f": "png", "fltr[]": [ "rcd|256", "mask|icons/icons.png" ] } ) );
 		return this._imageUrl( filename );
 	},
 