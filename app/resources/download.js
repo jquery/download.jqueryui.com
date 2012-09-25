a7347f744b3c3517c3c6c7dc8e4693d1315a6c77
Rafael Xavier de Souza <rxaviers@gmail.com>
2012-09-25 07:15:56 -0300

    Slide toggle Advanced Theme Settings

--- app/resources/download.js
+++ app/resources/download.js
@@ -219,6 +219,13 @@
 				$( "#theme-folder-name" ).removeData( "edited" );
 			}
 		});
+
+		$( ".advanced-settings-heading" ).click(function() {
+				$( this ).next().slideToggle();	
+				$( this ).find( "span" ).toggleClass( "ui-icon-triangle-1-s" );
+		})
+		.prepend( "<span class=\"ui-icon ui-icon-triangle-1-e\"></span>" )
+		.next().hide();
 	}, function( jqXHR, textStatus, errorThrown ) {
 		console.log( "Failed loading theme section", textStatus, errorThrown );
 	});
