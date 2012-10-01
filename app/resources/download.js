jquery/download.jqueryui.com

commit 5997733b91f652d0a06ea896d9ecb59d2f1c6138

Make Download Builder and ThemeRoller widths adjustable to smaller widths
- Replace fieldset with div, because fieldset does not behave properly as
  table-row;

app/resources/download.css
@@ -103,30 +103,31 @@
 	right: 15px;
 	top: 12px;
 }
-.download-builder fieldset {
+.download-builder .components {
 	display: table;
-	border: 0 none;
-	border-bottom: 1px dotted #666666;
-	outline: 0 none;
-	padding: 25px 15px 15px;
+	width: 100%;
 	position: relative;
 }
-.download-builder fieldset > div {
+.download-builder .components > div {
 	display: table-row;
+	border: 0 none;
 }
-.download-builder fieldset > div > div {
+.download-builder .components > div > div {
 	display: table-cell;
+	border-bottom: 1px dotted #666666;
+	padding: 25px 15px 15px;
 }
 .download-builder .component-group {
+	position: relative;
 	border-bottom: 1px dotted #666666;
 	padding: 25px 15px 15px;
-	position: relative;
 }
 .download-builder .component-group-desc {
-	width: 230px;
+	max-width: 230px;
+	width: 30%;
 }
 .download-builder .component-group-list {
-	min-width: 400px;
+	width: 70%;
 }
 .download-builder .component-group-desc h3 {
 	font-size: 1.5em;

app/resources/download.js
@@ -18,7 +18,7 @@
 	}
 
 	function allGroup( referenceElement ) {
-		return $( referenceElement ).closest( "fieldset" ).find( ".component-group-list input[type=checkbox]" );
+		return $( referenceElement ).closest( ".components" ).find( ".component-group-list input[type=checkbox]" );
 	}
 
 	function _check( elem, value ) {
@@ -47,7 +47,7 @@
 				if ( value ) {
 					// Set group toggle all if all components of its group are checked
 					if ( !allGroup( elem ).filter( ":not(:checked)" ).length ) {
-						$( elem ).closest( "fieldset" ).find( ".toggle input[type=checkbox]" ).prop( "checked", true );
+						$( elem ).closest( ".components" ).find( ".toggle input[type=checkbox]" ).prop( "checked", true );
 					}
 					// Set toggle all if all components are checked
 					if ( !allComponents( elem ).filter( ":not(:checked)" ).length ) {
@@ -56,7 +56,7 @@
 				} else {
 					// Unset group toggle all if no components of its group are checked
 					if ( !allGroup( elem ).filter( ":checked" ).length ) {
-						$( elem ).closest( "fieldset" ).find( ".toggle input[type=checkbox]" ).prop( "checked", false );
+						$( elem ).closest( ".components" ).find( ".toggle input[type=checkbox]" ).prop( "checked", false );
 					}
 					// Unset toggle all if no components are checked
 					if ( !allComponents( elem ).filter( ":checked" ).length ) {
@@ -174,7 +174,7 @@
 	});
 
 	// Generating toggle all checkboxes
-	$( ".download-builder .components h2" ).after( drawToggleAll( "toggleAll" ) );
+	$( ".download-builder .components" ).prev().find( "h2" ).after( drawToggleAll( "toggleAll" ) );
 	$( ".download-builder .components h3" ).after( drawToggleAll( "toggle" ) );
 
 	// binds click handlers on checkboxes

template/download/index.html
@@ -6,30 +6,28 @@
 			<h2>Version</h2>
 			<p class="component-title">{{pkg.version}}</p>
 		</div>
+		<div class="download-builder-header">
+			<h2>Components</h2>
+		</div>
 		<div class="components">
-			<div class="download-builder-header">
-				<h2>Components</h2>
-			</div>
 			{{#each categories}}
-				<fieldset class="ui-widget ui-widget-content">
-					<div>
-						<div class="component-group-desc">
-							<h3>{{name}}</h3>
-							<p>{{description}}</p>
-						</div>
-						<div class="component-group-list">
-							<div>
-							{{#each components}}
-								<label title="{{description}}">
-									<input type="checkbox" checked name="{{name}}" class="ui-widget-content" data-dependencies="{{dependencies}}">
-									<span class="component-title">{{title}}</span>
-									<span class="component-desc">{{description}}</span>
-								</label>
-							{{/each}}
-							</div>
+				<div class="ui-widget ui-widget-content">
+					<div class="component-group-desc">
+						<h3>{{name}}</h3>
+						<p>{{description}}</p>
+					</div>
+					<div class="component-group-list">
+						<div>
+						{{#each components}}
+							<label title="{{description}}">
+								<input type="checkbox" checked name="{{name}}" class="ui-widget-content" data-dependencies="{{dependencies}}">
+								<span class="component-title">{{title}}</span>
+								<span class="component-desc">{{description}}</span>
+							</label>
+						{{/each}}
 						</div>
 					</div>
-				</fieldset>
+				</div>
 			{{/each}}
 		</div>
 		<input type="submit" class="ui-widget-content" value="Download">

app/resources/themeroller.css
@@ -1,14 +1,5 @@
 .themeroller {
-	display: table;
 	font-size: 10px;
-}
-.themeroller > div {
-	display: table-row;
-}
-.themeroller > div > div {
-	display: table-cell;
-	vertical-align: top;
-	padding: 15px;
 }
 
 /* ui site overrides for TR */
@@ -44,6 +35,7 @@
 	color: #fff;
 	border: 1px solid #222;
 	position: relative;
+	float: left;
 }
 .themeroller .application a:focus, div.content * {
 	outline: 0 !important;
@@ -52,7 +44,7 @@
 	font-size: 1.5em;
 	color: #fff;
 	font-weight: normal;
-	width: 213;
+	width: 213px;
 	height: 46px;
 	margin: 0 0 -1px 0;
 	padding: 0;
@@ -493,6 +485,17 @@ a.cornerWarning { color: red !important; text-decoration: none; }
 }
 
 /* Components Column */
+.themeroller .components {
+	display: table;
+}
+.themeroller .components > div {
+	display: table-row;
+}
+.themeroller .components > div > div {
+	display: table-cell;
+	vertical-align: top;
+	padding-left: 15px;
+}
 #versionNotice {
 	margin: 20px 0;
 	border: 1px solid #333;
@@ -501,10 +504,11 @@ a.cornerWarning { color: red !important; text-decoration: none; }
 	background: #eee;
 }
 #compGroupA {
+	min-width: 216px;
 }
 
 #compGroupB {
-	width: 250px;
+	max-width: 250px;
 }
 .demoHeaders {
 	font-size: 1.3em;

commit a341fae945e6cf242c88b428171d6c64be716d78

Fix toggle vs. toggleAll
- regression bug introduced by 5997733b91f652d0a06ea896d9ecb59d2f1c6138

app/resources/download.css
@@ -113,12 +113,8 @@
 	border: 0 none;
 }
 .download-builder .components > div > div {
-	display: table-cell;
-	border-bottom: 1px dotted #666666;
-	padding: 25px 15px 15px;
-}
-.download-builder .component-group {
 	position: relative;
+	display: table-cell;
 	border-bottom: 1px dotted #666666;
 	padding: 25px 15px 15px;
 }

app/resources/download.js
@@ -18,7 +18,7 @@
 	}
 
 	function allGroup( referenceElement ) {
-		return $( referenceElement ).closest( ".components" ).find( ".component-group-list input[type=checkbox]" );
+		return $( referenceElement ).closest( ".component-group" ).find( ".component-group-list input[type=checkbox]" );
 	}
 
 	function _check( elem, value ) {
@@ -47,20 +47,20 @@
 				if ( value ) {
 					// Set group toggle all if all components of its group are checked
 					if ( !allGroup( elem ).filter( ":not(:checked)" ).length ) {
-						$( elem ).closest( ".components" ).find( ".toggle input[type=checkbox]" ).prop( "checked", true );
+						$( elem ).closest( ".component-group" ).find( ".toggle input[type=checkbox]" ).prop( "checked", true );
 					}
 					// Set toggle all if all components are checked
 					if ( !allComponents( elem ).filter( ":not(:checked)" ).length ) {
-						$( elem ).closest( "form" ).find( ".toggleAll input[type=checkbox]" ).prop( "checked", true );
+						$( elem ).closest( ".components" ).prev().find( ".toggleAll input[type=checkbox]" ).prop( "checked", true );
 					}
 				} else {
 					// Unset group toggle all if no components of its group are checked
 					if ( !allGroup( elem ).filter( ":checked" ).length ) {
-						$( elem ).closest( ".components" ).find( ".toggle input[type=checkbox]" ).prop( "checked", false );
+						$( elem ).closest( ".component-group" ).find( ".toggle input[type=checkbox]" ).prop( "checked", false );
 					}
 					// Unset toggle all if no components are checked
 					if ( !allComponents( elem ).filter( ":checked" ).length ) {
-						$( elem ).closest( "form" ).find( ".toggleAll input[type=checkbox]" ).prop( "checked", false);
+						$( elem ).closest( ".components" ).prev().find( ".toggleAll input[type=checkbox]" ).prop( "checked", false);
 					}
 				}
 			}
@@ -77,9 +77,11 @@
 			elem.each(function() {
 				var elem = $( this ),
 					name = elem.attr( "name" );
-				if ( name && dependents[ name ] && dependents[ name ].filter( ":checked" ).length > 0 ) {
+				if ( name ) {
 					consolidatedNames.push( name );
-					consolidatedDependents = consolidatedDependents.add( dependents[ name ].filter( ":checked" ) );
+					if ( dependents[ name ] ) {
+						consolidatedDependents = consolidatedDependents.add( dependents[ name ].filter( ":checked" ) );
+					}
 				}
 			});
 
@@ -175,7 +177,7 @@
 
 	// Generating toggle all checkboxes
 	$( ".download-builder .components" ).prev().find( "h2" ).after( drawToggleAll( "toggleAll" ) );
-	$( ".download-builder .components h3" ).after( drawToggleAll( "toggle" ) );
+	$( ".download-builder .component-group h3" ).after( drawToggleAll( "toggle" ) );
 
 	// binds click handlers on checkboxes
 	$( ".download-builder input[type=checkbox]" ).click(function( event ) {
@@ -183,7 +185,7 @@
 		if ( target.parent().is( ".toggle" ) ) {
 			check( allGroup( this ), $( this ).prop( "checked" ) );
 		} else if ( target.parent().is( ".toggleAll" ) ) {
-			check( allComponents( this ).not( this ), $( this ).prop( "checked" ) );
+			check( allComponents( this ), $( this ).prop( "checked" ) );
 		} else {
 			check( $( this ), $( this ).prop( "checked" ) );
 		}

template/download/index.html
@@ -11,7 +11,7 @@
 		</div>
 		<div class="components">
 			{{#each categories}}
-				<div class="ui-widget ui-widget-content">
+				<div class="ui-widget ui-widget-content component-group">
 					<div class="component-group-desc">
 						<h3>{{name}}</h3>
 						<p>{{description}}</p>