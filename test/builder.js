e10c6d8b6aa5121ab83fc91513b8aebab48ab967
Rafael Xavier de Souza <rxaviers@gmail.com>
2013-01-07 17:59:59 -0200

    Revert "Temporary change for 1.10.0pre testing purposes on stage"
    This reverts commit d7aa4260a7ba75aa907fa9a7b090d1ee51dcbb68.

--- test/builder.js
+++ test/builder.js
@@ -520,8 +520,7 @@ module.exports = {};
 // Build tests for each jqueryUi release
 Release.all().filter(function( release ) {
 	// Filter supported releases only
-	// FIXME below s/1.10.0pre/1.10.0. This is a temporary test change.
-	return semver.gte( release.pkg.version, "1.10.0pre" );
+	return semver.gte( release.pkg.version, "1.10.0" );
 }).forEach(function( release ) {
 	function deepTestBuild( obj, tests ) {
 		Object.keys( tests ).forEach(function( i ) {
