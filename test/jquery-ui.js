92a6928af4f519d7fed5898ead167353df52ddd1
Rafael Xavier de Souza <rxaviers@gmail.com>
2013-05-14 14:11:41 -0300

    Make each file of JqueryUiFiles a {path, data} key-pair value. Allows chaining more straightforward
    - Along with related changes, builder helper add method changed as below:
      add( src, dst, [ data ] ) -> add( file ) or add( src, data )

--- test/jquery-ui.js
+++ test/jquery-ui.js
@@ -14,40 +14,40 @@ var tests = {
 	"test: common files present": function( test ) {
 		var files = this.jqueryUi.files();
 		test.expect( files.commonFiles.length );
-		files.commonFiles.forEach(function( path ) {
-			test.ok( notNull( files.data[ path ] ), "Null file \"" + path + "\"." );
+		files.commonFiles.forEach(function( file ) {
+			test.ok( notNull( file.data ), "Null file \"" + file.path + "\"." );
 		});
 		test.done();
 	},
 	"test: component files present": function( test ) {
 		var files = this.jqueryUi.files();
 		test.expect( files.componentFiles.length );
-		files.componentFiles.forEach(function( path ) {
-			test.ok( notNull( files.data[ path ] ), "Null file \"" + path + "\"." );
+		files.componentFiles.forEach(function( file ) {
+			test.ok( notNull( file.data ), "Null file \"" + file.path + "\"." );
 		});
 		test.done();
 	},
 	"test: demo files present": function( test ) {
 		var files = this.jqueryUi.files();
 		test.expect( files.demoFiles.length );
-		files.demoFiles.forEach(function( path ) {
-			test.ok( notNull( files.data[ path ] ), "Null file \"" + path + "\"." );
+		files.demoFiles.forEach(function( file ) {
+			test.ok( notNull( file.data ), "Null file \"" + file.path + "\"." );
 		});
 		test.done();
 	},
 	"test: doc files present": function( test ) {
 		var files = this.jqueryUi.files();
 		test.expect( files.docFiles.length );
-		files.docFiles.forEach(function( path ) {
-			test.ok( notNull( files.data[ path ] ), "Null file \"" + path + "\"." );
+		files.docFiles.forEach(function( file ) {
+			test.ok( notNull( file.data ), "Null file \"" + file.path + "\"." );
 		});
 		test.done();
 	},
 	"test: theme files present": function( test ) {
 		var files = this.jqueryUi.files();
 		test.expect( files.themeFiles.length );
-		files.themeFiles.forEach(function( path ) {
-			test.ok( notNull( files.data[ path ] ), "Null file \"" + path + "\"." );
+		files.themeFiles.forEach(function( file ) {
+			test.ok( notNull( file.data ), "Null file \"" + file.path + "\"." );
 		});
 		test.done();
 	}
