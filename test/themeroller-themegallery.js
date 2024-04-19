f542257d4b7d2c292711b52108c0d59051053118
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2024-04-19 17:03:37 +0200

    Themeroller: Fix stale cachedZThemeParams in `themeroller-themegallery`
    Also, add a test for their correctness.
    
    Fixes gh-602
    Closes gh-609

---
+++ test/themeroller-themegallery.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const fs = require( "fs" );
+const zParams = require( "../lib/zparams" );
+const { themeGalleryData } = require( "../lib/themeroller-themegallery" );
+
+function omit( obj, keys ) {
+	let key;
+	const copy = Object.create( null );
+	for ( key in obj ) {
+		if ( !keys.includes( key ) ) {
+			copy[ key ] = obj[ key ];
+		}
+	}
+	return copy;
+}
+
+module.exports = {
+	"test: cachedZThemeParams in themeGalleryData": {
+		"cachedZThemeParams properly computed": function( test ) {
+			Promise
+				.all( themeGalleryData.map( ( { vars, cachedZThemeParams } ) =>
+					new Promise( function( resolve ) {
+						zParams.unzip( cachedZThemeParams, ( unzippedVars ) => {
+							test.deepEqual(
+								unzippedVars,
+								omit( vars, [ "name" ] ),
+								`Decompressed values should match for theme ${ vars.name }`
+							);
+							resolve();
+						} );
+					} )
+				) )
+				.then( () => {
+					test.done();
+				} );
+		}
+	}
+};
