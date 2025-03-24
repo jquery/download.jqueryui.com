a73e873cedb8dbc2f325590bcdcff5385119e731
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2025-03-24 18:05:23 +0100

    Themeroller: Fix alpha treatment in icons
    Since jQuery UI 1.13.0, partially transparent pixels in the icons file started
    being too dark. Set gamma to 0.5 for the A channel to get closer to the 1.12.1
    output.
    
    A test comparing similarity of the generated image to the jQuery 1.12.1 has been
    added as well.
    
    Fixes jquery/jquery-ui#2339
    Closes gh-635

---
+++ test/lib/compare-images.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const fs = require( "node:fs" );
+const PNG = require( "pngjs" ).PNG;
+
+function compareParsedImages( imgA, imgE, delta = 0 ) {
+
+	// Ensure both images have the same dimensions
+	if ( imgA.width !== imgE.width || imgA.height !== imgE.height ) {
+		return {
+			passed: false,
+			message: "Images have different dimensions"
+		};
+	}
+
+	// Loop over every pixel (each pixel occupies 4 positions in the data array)
+	for ( let y = 0; y < imgA.height; y++ ) {
+		for ( let x = 0; x < imgA.width; x++ ) {
+
+			// Calculate the starting index for this pixel in the buffer using multiplication
+			const i = ( imgA.width * y + x ) * 4;
+			const rgbaA = {
+				r: imgA.data[ i ],
+				g: imgA.data[ i + 1 ],
+				b: imgA.data[ i + 2 ],
+				alpha: imgA.data[ i + 3 ]
+			};
+			const rgbaE = {
+				r: imgE.data[ i ],
+				g: imgE.data[ i + 1 ],
+				b: imgE.data[ i + 2 ],
+				alpha: imgE.data[ i + 3 ]
+			};
+			const rDiff = Math.abs( rgbaA.r - rgbaE.r );
+			const gDiff = Math.abs( rgbaA.g - rgbaE.g );
+			const bDiff = Math.abs( rgbaA.b - rgbaE.b );
+			const alphaDiff = Math.abs( rgbaA.alpha - rgbaE.alpha );
+
+			if ( rDiff > delta || gDiff > delta || bDiff > delta || alphaDiff > delta ) {
+				return {
+					passed: false,
+					message: `Error at pixel (x: ${ x }, y: ${ y }): ` +
+						`Image actual: rgba(${ rgbaA.r }, ${ rgbaA.g }, ${ rgbaA.b }, ${ rgbaA.alpha }) vs. ` +
+						`Image expected: rgba(${ rgbaE.r }, ${ rgbaE.g }, ${ rgbaE.b }, ${ rgbaE.alpha })`
+				};
+			}
+		}
+	}
+
+	return {
+		passed: true,
+		message: "Images sufficiently similar"
+	};
+}
+
+async function compareImages( pathActual, pathExpected, delta = 0 ) {
+	return new Promise( async( resolve, reject ) => {
+		const bufferActual = fs.readFileSync( pathActual );
+		const bufferExpected = fs.readFileSync( pathExpected );
+		const [ imgActual, imgExpected ] = await Promise
+			.all(
+				[
+					bufferActual,
+					bufferExpected
+				].map( buffer => new Promise( ( res ) => {
+					new PNG( { filterType: 4 } )
+						.parse( buffer, function( error, img ) {
+							if ( error ) {
+								reject( error );
+								return;
+							}
+							res( img );
+						} );
+					}
+				) )
+			);
+
+		resolve( compareParsedImages( imgActual, imgExpected, delta ) );
+	} );
+}
+
+module.exports = { compareImages };
---
+++ test/themeroller-image.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const fs = require( "node:fs/promises" );
+const path = require( "node:path" );
+const Image = require( "../lib/themeroller-image" );
+const { compareImages } = require( "./lib/compare-images" );
+
+const fixturesPath = path.join( __dirname, "fixtures" );
+const tmpFolderPath = path.join( __dirname, "tmp" );
+
+QUnit.module( "themeroller-image", {
+	beforeEach: async function() {
+		await fs.rm( tmpFolderPath, {
+			recursive: true,
+			force: true
+		} );
+		await fs.mkdir( tmpFolderPath, { recursive: true } );
+	},
+	afterEach: async function() {
+
+		// await fs.rm( tmpFolderPath, {
+		// 	recursive: true,
+		// 	force: true
+		// } );
+	}
+} );
+
+QUnit.test( "generates correct icons", async function( assert ) {
+	const done = assert.async();
+	assert.expect( 1 );
+
+	const fixturePath = path.join( fixturesPath,
+		"images/ui-icons_444444_256x240.png" );
+
+	const image = new Image( {
+		icon: { color: "#444444" }
+	} );
+
+	image.get( async function( err, filename, data ) {
+		if ( err ) {
+			assert.ok( false, `Error: ${ err && err.message || err }` );
+			done();
+			return;
+		}
+
+		const iconsPath = path.join( tmpFolderPath, filename );
+		await fs.writeFile( iconsPath, data );
+
+		const { passed, message } = await compareImages( iconsPath, fixturePath, 2 );
+		assert.ok( passed, message );
+
+		done();
+	} );
+} );
+
